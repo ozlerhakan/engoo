299155dd0c01d368d874ef92d96e43217bd63e73
Hakan Özler <ozler.hakan@gmail.com>
2024-05-08 21:59:59 +0300

    sync dom changes

--- content.js
+++ content.js
@@ -1,33 +1,39 @@
 
 
 window.onload = function () {
-    const timer = setInterval(() => {
+    const intervalId = setInterval(() => {
         const vocabularies = document.querySelectorAll("span.css-18tpl2u")
         if (vocabularies && vocabularies.length != 0) {
-            clearTimeout(timer);
+            clearTimeout(intervalId);
 
             let text = '';
             vocabularies.forEach(function (element, index) {
-                text += element.textContent;
+                text += '( ' + element.textContent + ')';
                 if (index < vocabularies.length - 1) {
                     text += '|'
                 }
             });
             const re = new RegExp(text, 'ig');
-            const spans = document.querySelector('#windowexercise-2 > div > div > div > div:nth-child(3) > div')
-            for (const element of spans.children) {
-                if (element.tagName != 'P') {
-                    continue
+            let spans = document.querySelector('#windowexercise-2 > div > div > div:nth-child(3) > div > div:nth-child(2)')
+            if (spans == undefined) {
+                spans = document.querySelector('#windowexercise-2 > div > div > div:nth-child(2) > div')
+                if (spans == undefined) {
+                    return;
                 }
+            }
+            for (const element of spans.children) {
+                // if (element.tagName != 'P') {
+                // continue
+                // }
                 const allMatchItr = [...element.innerText.trim().matchAll(re)]
-                let innerElementText = element.innerText
+                let innerElementText = element.outerHTML
                 for (match of allMatchItr) {
-                    innerElementText = innerElementText.replace(match[0], "<b>" + match[0] + "</b>")
+                    innerElementText = innerElementText.replace(match[0], " <b style='font-site:40px; font-family: Helvetica,Arial,sans-serif;text-decoration:none;color:#ffffff;padding:0px4px;display:inline-block;background-color:#eb4034'>" + match[0] + "</b> ")
                 }
                 element.innerHTML = innerElementText
             }
         } else {
-            console.log("cannot find the element, querying again in 150ms.");
+            console.log("cannot find the element, querying again in 2000ms.");
         }
-    }, 1500);
+    }, 2000);
 }
